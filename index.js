4e6a6f195cdfc9a90c3c99e715e5db39914af51b
ahmed atta <ahmedatta3322@gmail.com>
2018-08-14 14:42:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ function subtract(a,b){
   
 }
 function multiply(a,b){
-  return a + b
+  return a * b
   
 }
 function divide(a,b){
-  return a + b
+  return a / b
+  
+}
+function inc(n){
+  return n += 1 
   
 }
